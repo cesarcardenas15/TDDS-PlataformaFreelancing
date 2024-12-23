d6365e13bf57ee95016e7f16a0491f146c9ee79e
cesarcardenas15 <148886026+cesarcardenas15@users.noreply.github.com>
2024-12-23 19:07:10 -0300

    Aplicar correcciones a gestión de sesión en frontend

--- frontend/src/components/navbar/UserLoggedIcon.jsx
+++ frontend/src/components/navbar/UserLoggedIcon.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import DropDownProfile from './UserDropdown';
-
+import { User } from 'lucide-react';
 function UserLoggedIcon() {
   const [openProfile, setOpenProfile] = useState(false);
-    return (
-      <div className="flex -space-x-1 overflow-hidden">
-        <img
-          alt=""
-          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          className="inline-block size-6 rounded-full ring-2 ring-white"
-          onClick={() => setOpenProfile(prev => !prev)}
-        />
-        { openProfile && <DropDownProfile/> }
-      </div>
-    );
+  return (
+    <div className="flex -space-x-1 overflow-hidden">
+      <button className="flex items-center space-x-2 bg-indigo-500 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition duration-300 ease-in-out inline-block" onClick={() => setOpenProfile(prev => !prev)}>
+        <User size={20} />
+      </button>
+      {openProfile && <DropDownProfile />}
+    </div>
+  );
 }
 
 export default UserLoggedIcon;
