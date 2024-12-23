4d20a728d8efa5168c9e41f270cf5fa272207458
cesarcardenas15 <148886026+cesarcardenas15@users.noreply.github.com>
2024-12-23 15:10:14 -0300

    Revert "Revert "Crear dropdown de sesión iniciada""
    This reverts commit a5161aa55e36b29fcdd25841d0ec2e2c212be5d3.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Search, Bell, User } from 'lucide-react';
+import { Search, Bell } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import UserLoggedIcon from './navbar/UserLoggedIcon';
+import UserLoginButtons from './navbar/UserLoginButtons';
 
 function Navbar() {
+  let accessToken = localStorage.getItem('access_token');
+  //let refreshToken = localStorage.getItem('refresh_token');
+  //let user = localStorage.getItem('user');
+
+  let userManage;
+  
+  if (accessToken) {
+    userManage = <UserLoggedIcon/>;
+  }
+  else {
+    userManage = <UserLoginButtons/>;
+  }
+
+  
   return (
     <nav className="bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -25,19 +41,7 @@ function Navbar() {
             <button className="p-2 text-gray-600 hover:text-indigo-600 transition duration-300 ease-in-out inline-block">
               <Bell size={20} />
             </button>
-            <div className="flex items-center space-x-2">
-              <Link to="/login">
-                <button className="flex items-center space-x-2 bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition duration-300 ease-in-out inline-block">
-                  <User size={20} />
-                  <span>Iniciar Sesión</span>
-                </button>
-              </Link>
-              <Link to="/signup">
-                <button className="flex items-center space-x-2 border border-indigo-600 text-indigo-600 px-4 py-2 rounded-lg hover:bg-indigo-600 hover:text-white transition duration-300 ease-in-out inline-block">
-                  <span>Registrarse</span>
-                </button>
-              </Link>
-            </div>
+            {userManage}
           </div>
         </div>
       </div>
